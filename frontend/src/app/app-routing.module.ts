e0c7bbd90db5f777488bfe2e4f3ad7651078039b
johainatounant <110460124+johainatounant@users.noreply.github.com>
2022-08-02 18:36:17 +0100

    navbar 1

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'myway',
+    loadChildren: () => import('../app/may-way/may-way.module').then(m => m.MayWayModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/may-way/may-way-routing.module.ts
+++ frontend/src/app/may-way/may-way-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'navbar', 
+  component:NavbarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
